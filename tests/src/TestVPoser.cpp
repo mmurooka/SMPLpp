3e88c008c98ef382db2d973e8493b730d0ff123e
Masaki Murooka <m-murooka@aist.go.jp>
2022-10-10 15:20:05 +0900

    [WIP] Add tests.

---
+++ tests/src/TestVPoser.cpp
@@ -0,0 +1,16 @@
+/* Author: Masaki Murooka */
+
+#include <gtest/gtest.h>
+
+#include <smpl/VPoser.h>
+
+TEST(TestDdpZmp, Test1)
+{
+  smpl::VPoserDecoder vposer;
+}
+
+int main(int argc, char ** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
