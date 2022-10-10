ad758baed65d1b1d3d8430e850e0c13adf89206b
Masaki Murooka <m-murooka@aist.go.jp>
2022-10-10 14:55:21 +0900

    [WIP] Add VPoser.

---
+++ include/smpl/VPoser.h
@@ -0,0 +1,48 @@
+/* Author: Masaki Murooka */
+
+#ifndef VPOSER_H
+#define VPOSER_H
+
+#include <torch/torch.h>
+
+namespace smpl
+{
+/** \brief VPoser decoder model.
+
+    Corresponds to decoder model in
+   https://github.com/nghorbani/human_body_prior/blob/master/src/human_body_prior/models/vposer_model.py
+*/
+class VPoserDecoderImpl : public torch::nn::Module
+{
+public:
+  /** \brief Constructor. */
+  VPoserDecoderImpl();
+
+  /** \brief Forward model.
+      \param latent latent variables
+      \returns joint angles
+  */
+  torch::Tensor forward(const torch::Tensor & latent);
+
+public:
+  //! Dimension of latent variables
+  const int64_t latentDim_ = 32;
+
+  //! Dimension of hidden variables
+  const int64_t hiddenDim_ = 512;
+
+  //! Number of joints
+  const int64_t jointNum_ = 21;
+
+  //! Decoder net
+  torch::nn::Sequential decoderNet_;
+};
+
+/** \brief VPoser decoder model pointer.
+
+    See "Module Ownership" section of https://pytorch.org/tutorials/advanced/cpp_frontend.html for details.
+*/
+TORCH_MODULE(VPoserDecoder);
+} // namespace smpl
+
+#endif // VPOSER_H
---
+++ src/smpl/VPoser.cpp
@@ -0,0 +1,28 @@
+/* Author: Masaki Murooka */
+
+#include <smpl/VPoser.h>
+
+using namespace smpl;
+
+VPoserDecoderImpl::VPoserDecoderImpl()
+{
+  // clang-format off
+  decoderNet_ = register_module(
+      "decoderNet",
+      torch::nn::Sequential(torch::nn::Linear(latentDim_, hiddenDim_),
+                            torch::nn::LeakyReLU(),
+                            torch::nn::Dropout(0.1),
+                            torch::nn::Linear(hiddenDim_, hiddenDim_),
+                            torch::nn::LeakyReLU(),
+                            torch::nn::Linear(hiddenDim_, 6 * jointNum_) // ,
+                            // ContinousRotReprDecoder() // \todo
+                            ));
+  // clang-format on
+}
+
+torch::Tensor VPoserDecoderImpl::forward(const torch::Tensor & latent)
+{
+  int64_t batchSize = latent.sizes()[0];
+  return decoderNet_->forward(latent);
+  // return matrot2aa(decoderNet_(latent).view({-1, 3, 3})).view({batchSize, -1, 3}); // \todo
+}
