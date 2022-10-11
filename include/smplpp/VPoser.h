f34b57b2e337400900565cc52d2cb8fd5960dd5c
Masaki Murooka <m-murooka@aist.go.jp>
2022-10-11 13:58:08 +0900

    Implement ContinousRotReprDecoder.

--- include/smplpp/VPoser.h
+++ include/smplpp/VPoser.h
@@ -9,18 +9,43 @@ namespace smplpp
 {
 /** \brief VPoser decoder model.
 
-    Corresponds to decoder model in
-   https://github.com/nghorbani/human_body_prior/blob/master/src/human_body_prior/models/vposer_model.py
+    Corresponds to the model in
+    https://github.com/nghorbani/human_body_prior/blob/master/src/human_body_prior/models/vposer_model.py
 */
 class VPoserDecoderImpl : public torch::nn::Module
 {
+public:
+  /** \brief Model to transform two 3D vectors to rotation matrices.
+
+      Corresponds to the model in
+      https://github.com/nghorbani/human_body_prior/blob/master/src/human_body_prior/models/vposer_model.py
+  */
+  class ContinousRotReprDecoderImpl : public torch::nn::Module
+  {
+  public:
+    /** \brief Constructor. */
+    ContinousRotReprDecoderImpl();
+
+    /** \brief Forward model.
+        \param input tensor representing two 3D vectors
+        \returns tensor representing rotation matrices
+    */
+    torch::Tensor forward(const torch::Tensor & input);
+  };
+
+  /** \brief Model pointer to transform two 3D vectors to rotation matrices.
+
+      See "Module Ownership" section of https://pytorch.org/tutorials/advanced/cpp_frontend.html for details.
+  */
+  TORCH_MODULE(ContinousRotReprDecoder);
+
 public:
   /** \brief Constructor. */
   VPoserDecoderImpl();
 
   /** \brief Forward model.
-      \param latent latent variables
-      \returns joint angles
+      \param latent tensor representing latent variables
+      \returns tensor representing joint angles
   */
   torch::Tensor forward(const torch::Tensor & latent);
 
--- src/VPoser.cpp
+++ src/VPoser.cpp
@@ -4,6 +4,27 @@
 
 using namespace smplpp;
 
+VPoserDecoderImpl::ContinousRotReprDecoderImpl::ContinousRotReprDecoderImpl()
+{
+  // Workaround to avoid torch error
+  // See https://github.com/pytorch/pytorch/issues/35736#issuecomment-688078143
+  torch::cuda::is_available();
+}
+
+torch::Tensor VPoserDecoderImpl::ContinousRotReprDecoderImpl::forward(const torch::Tensor & input)
+{
+  torch::Tensor reshapedInput = input.view({-1, 3, 2});
+  torch::Tensor col1 = reshapedInput.index({at::indexing::Slice(), at::indexing::Slice(), 0});
+  torch::Tensor col2 = reshapedInput.index({at::indexing::Slice(), at::indexing::Slice(), 1});
+
+  torch::Tensor axis1 = torch::nn::functional::normalize(col1, torch::nn::functional::NormalizeFuncOptions().dim(1));
+  torch::Tensor axis2 = torch::nn::functional::normalize(col2 - (axis1 * col2).sum(1, true) * axis1,
+                                                         torch::nn::functional::NormalizeFuncOptions().dim(-1));
+  torch::Tensor axis3 = at::native::cross(axis1, axis2, 1);
+
+  return at::native::stack({axis1, axis2, axis3}, -1);
+}
+
 VPoserDecoderImpl::VPoserDecoderImpl()
 {
   // clang-format off
@@ -14,10 +35,14 @@ VPoserDecoderImpl::VPoserDecoderImpl()
                             torch::nn::Dropout(0.1),
                             torch::nn::Linear(hiddenDim_, hiddenDim_),
                             torch::nn::LeakyReLU(),
-                            torch::nn::Linear(hiddenDim_, 6 * jointNum_) // ,
-                            // ContinousRotReprDecoder() // \todo
+                            torch::nn::Linear(hiddenDim_, 6 * jointNum_),
+                            ContinousRotReprDecoder()
                             ));
   // clang-format on
+
+  // Workaround to avoid torch error
+  // See https://github.com/pytorch/pytorch/issues/35736#issuecomment-688078143
+  torch::cuda::is_available();
 }
 
 torch::Tensor VPoserDecoderImpl::forward(const torch::Tensor & latent)
