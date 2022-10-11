a7919d92e52ebec6b0bbdf7fd44a5a86d9eb07f7
Masaki Murooka <m-murooka@aist.go.jp>
2022-10-11 12:15:05 +0900

    Print tensor in test.

--- tests/src/TestVPoser.cpp
+++ tests/src/TestVPoser.cpp
@@ -7,6 +7,11 @@
 TEST(TestDdpZmp, Test1)
 {
   smplpp::VPoserDecoder vposer;
+  torch::Tensor vposerIn = torch::rand({1, vposer->latentDim_});
+  torch::Tensor vposerOut = vposer->forward(vposerIn);
+
+  std::cout << "vposerIn:\n" << vposerIn << std::endl;
+  std::cout << "vposerOut:\n" << vposerOut << std::endl;
 }
 
 int main(int argc, char ** argv)
